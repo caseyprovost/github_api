3c4d7e2a2f6e7e65a5208dddd9421c8ad36cdfa2
Casey Provost <caseyr.provost@gmail.com>
2019-10-05 14:29:46 -0400

    setup db

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,4 @@ Rails.application.routes.draw do
     mount VandalUi::Engine, at: "/vandal"
     # your routes go here
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
