28ae467d0e962384c6af5f4a575e63edc4275574
Casey Provost <caseyr.provost@gmail.com>
2019-10-06 00:48:39 -0400

    add gists to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ApplicationRecord
 
   has_many :followings, foreign_key: :target_id
   has_many :followers, through: :followings, class_name: "User"
-
   has_many :peer_followings, class_name: "Following", foreign_key: :follower_id
   has_many :followed_users, through: :peer_followings, class_name: "User", source: :target
+  has_many :gists, foreign_key: :owner_id
 end
